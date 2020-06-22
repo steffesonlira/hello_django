b505bc187e17300ff02efdec8b0909cb5b3a87f5
steffesonlira <steffesonlira>
2020-06-22 11:39:05 -0300

    exercicio soma com HttpRequest

--- core/views.py
+++ core/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+
+def soma(request, a, b, soma):
+    soma = a + b
+    return HttpResponse('<h2>A soma entre {} e {} é: {som}'.format(a,b,som=soma))
